f71fbd74d016d1a0b24491d9d1d97b912e8872f4
dargaCode <darga.code@gmail.com>
2017-02-24 11:01:51 -0800

    Data types

---
+++ 01_thirty-days-of-code/01_data-types.js
@@ -0,0 +1,26 @@
+
+// https://www.hackerrank.com/challenges/30-data-types
+
+// Given an int, a double, and a string, add / concatenate them with provided values and print them out.
+
+// CONSTANTS
+
+const STARTING_INT = 6;
+const STARTING_DOUBLE = 4.0;
+const STARTING_STR = 'is the best black to learn and practice coding';
+
+// MAIN
+
+(function main() {
+  const int = 12;
+  const double = 4.0;
+  const str = 'HackerRank';
+
+  const resultInt = int + STARTING_INT;
+  const resultDouble = double + STARTING_DOUBLE;
+  const resultStr = str + STARTING_STR;
+
+  console.log(resultInt);
+  console.log(resultDouble);
+  console.log(resultStr);
+}());
